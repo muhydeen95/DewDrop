a851effee9f84958ee80375cf79473db22a6ce59
Alabi Muhydeen Olaniyi <alabimuhydeen@gmail.com>
2020-07-24 10:27:41 +0100

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+// Navbar button hide and show
+
+let navOpen = document.querySelector('.navbar-open');
+let navClose = document.querySelector('.navbar-close');
+
+
+function toggleBtn() {
+    if (navClose.classList.contains('d-none')) {
+        navClose.classList.remove('d-none');
+        navOpen.classList.add('d-none');
+    } else {
+        navClose.classList.add('d-none');
+        navOpen.classList.remove('d-none');
+    }
+}
+
+
+navOpen.addEventListener('click', toggleBtn);
+
+
+// AOS Animation
+AOS.init({
+    duration: 1000,// values from 0to 3000, with step 50ms
+});
